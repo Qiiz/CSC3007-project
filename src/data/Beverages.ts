0b0584b2ad9ba63943c3f2f77215135f77f36df1
Chen Yu <scheduledx@gmail.com>
2022-07-27 00:18:50 +0800

    add catergory icons

--- src/data/Beverages.ts
+++ src/data/Beverages.ts
@@ -11,399 +11,399 @@ export const Beverages: Beverage[] = [
         "name": "Green Tea",
         "calories": 0,
         "caffeine": 25,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Pepsi One",
         "calories": 1,
         "caffeine": 55,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/soft-drinks.png",
         "category": "SOFT DRINK"
     },
     {
         "name": "Earl Grey",
         "calories": 2,
         "caffeine": 58,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Instant Coffee",
         "calories": 4,
         "caffeine": 47,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Decaf espresso",
         "calories": 5,
         "caffeine": 25,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Espresso",
         "calories": 5,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Yerba Mate",
         "calories": 5,
         "caffeine": 85,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Dark Roast",
         "calories": 5,
         "caffeine": 260,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Double Espresso",
         "calories": 6,
         "caffeine": 130,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Espresso Machiato ",
         "calories": 9,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Americano",
         "calories": 15,
         "caffeine": 225,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Iced Americano",
         "calories": 15,
         "caffeine": 225,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Arabic Coffee",
         "calories": 16,
         "caffeine": 100,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Lungo",
         "calories": 25,
         "caffeine": 60,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Americano + milk",
         "calories": 30,
         "caffeine": 225,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "English Breakfast + 1 sugar",
         "calories": 39,
         "caffeine": 26,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Marocchino ",
         "calories": 43,
         "caffeine": 90,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Turkish Coffee ",
         "calories": 46,
         "caffeine": 25,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Red Bull",
         "calories": 46,
         "caffeine": 77,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/soft-drinks.png",
         "category": "SOFT DRINK"
     },
     {
         "name": "English Breakfast + 2 sugars",
         "calories": 55,
         "caffeine": 26,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Marocchino Bicolore ",
         "calories": 55,
         "caffeine": 75,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Instant Coffee + 2 sugars",
         "calories": 83,
         "caffeine": 47,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Espresso Con Panna ",
         "calories": 89,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Caffè Shakerato ",
         "calories": 100,
         "caffeine": 225,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Café Crème",
         "calories": 106,
         "caffeine": 225,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Estivo ",
         "calories": 120,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Cappuccino ",
         "calories": 120,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Karak Tea",
         "calories": 122,
         "caffeine": 48,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Iced Latte ",
         "calories": 130,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Pepsi",
         "calories": 138.6,
         "caffeine": 38,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/soft-drinks.png",
         "category": "SOFT DRINK"
     },
     {
         "name": "Cannellino ",
         "calories": 143,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Coke ",
         "calories": 150,
         "caffeine": 32,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/soft-drinks.png",
         "category": "SOFT DRINK"
     },
     {
         "name": "Caramella Freddo ",
         "calories": 165,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Latte",
         "calories": 190,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Viennese ",
         "calories": 200,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Mocha (small)",
         "calories": 220,
         "caffeine": 195,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Green Tea Latte ",
         "calories": 240,
         "caffeine": 80,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Chai Latte ",
         "calories": 240,
         "caffeine": 95,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/tea.png",
         "category": "TEA"
     },
     {
         "name": "Iced Caramel Macchiato ",
         "calories": 250,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Cinnamon Vanilla Latté ",
         "calories": 250,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Torroncino ",
         "calories": 256,
         "caffeine": 90,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Lucozade",
         "calories": 266,
         "caffeine": 46,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/soft-drinks.png",
         "category": "SOFT DRINK"
     },
     {
         "name": "Bananino ",
         "calories": 268,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Cremino ",
         "calories": 276,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Caramel Frappuccino (med)",
         "calories": 280,
         "caffeine": 100,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Mountain Dew",
         "calories": 290,
         "caffeine": 91,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/soft-drinks.png",
         "category": "SOFT DRINK"
     },
     {
         "name": "Nocciolino ",
         "calories": 327,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Marocchino Speciale",
         "calories": 360,
         "caffeine": 175,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Vanillino ",
         "calories": 375,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Vanilla Cream Frappuccino ",
         "calories": 400,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Caramel Frappuccino (lrg)",
         "calories": 410,
         "caffeine": 100,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Frappuccino ",
         "calories": 410,
         "caffeine": 110,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Mocha Choc Chip Frap",
         "calories": 420,
         "caffeine": 15,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Caramel Sticky Mocha ",
         "calories": 420,
         "caffeine": 110,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Spanish Blend ",
         "calories": 425,
         "caffeine": 25,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "White Mocha ",
         "calories": 430,
         "caffeine": 65,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     },
     {
         "name": "Mocha (big)",
         "calories": 430,
         "caffeine": 150,
-        "icon": "./img/coffee/coffee-cup.png",
+        "icon": "./img/beverages/coffee-beans.png",
         "category": "COFFEE"
     }
 ]
